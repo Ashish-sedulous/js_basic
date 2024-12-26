a7ab5312309b9ba5769627219321ff67dd5b5137
Ashish <ashishkumar12340sss@gmail.com>
2024-12-26 08:56:31 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+console.log("hello");
+const valueid=4545;
+let bhimid=1246;
+bhimid=14526;
+console.log(bhimid);
+let modi;
+let arvind=4589;
+console.table(valueid,bhimid,arvind,modi); 
+console.log(typeof undefined);
+console.log(typeof null);
+
+//type of null->object
+//type of undefined->undefined
