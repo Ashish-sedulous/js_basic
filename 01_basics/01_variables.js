ec82536016c20dd68559e818b97f057f8b1e179f
Ashish <ashishkumar12340sss@gmail.com>
2024-12-27 10:51:33 +0000

    datatypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -5,9 +5,12 @@ bhimid=14526;
 console.log(bhimid);
 let modi;
 let arvind=4589;
-console.table(valueid,bhimid,arvind,modi); 
-console.log(typeof undefined);
-console.log(typeof null);
+console.table([valueid,bhimid,arvind,modi]); 
+console.log(typeof(undefined));
+console.log(typeof(null));
+let loggedin=1;
+let booleanin= Boolean(loggedin)
+console.log(booleanin);
 
 //type of null->object
 //type of undefined->undefined
--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,36 @@
-console.log("ashish"); 
+// let somenumber=34
+
+// let stringnumber= String(somenumber)
+// console.log(stringnumber);
+// console.log(typeof(stringnumber));
+
+//     operations
+
+// let value=9
+// let negvalue=-value
+// console.log(negvalue)
+
+// console.log(3+5)
+// console.log(3-5)
+// console.log(3*5)
+// console.log(3/5)
+// console.log(3%5)
+// console.log(3**5)
+
+// let str1="ashish"
+// let str2=" chaudhary"
+// console.log(str1+str2);
+
+// console.log("3"+5);
+// console.log(3+"5");
+// console.log("3"+5+4);
+// console.log(3+5+"2");
+
+// console.log(true);  //output-> true
+// console.log(+true); //output->1
+// console.log(+""); //output->0
+
+// console.log("2"===2); //=== means strictly check also check the data types result is false
+
+//== >= <= and < > = works a little diferently
+
