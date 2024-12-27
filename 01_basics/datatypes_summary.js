9f23197ef8e694deb2ade807536eb6d094c20989
Ashish <ashishkumar12340sss@gmail.com>
2024-12-27 10:51:03 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,27 @@
+//two types of datatypes->primitive and non-primitive
+
+// primitive 
+// 7 types-> string, number, null, undefined, symbol,boolean,Bigint
+
+//refernece(non primitive)
+// array,objects,functions
+
+// const id=Symbol('123')
+// const anotherid=Symbol('123')
+
+// console.log(id==anotherid); //output->false
+
+// const bignumber=5676165479615646n //Bigint
+
+// const arr=["shaktiman","nagraj","doga"]; //array
+
+// let myobj={
+//     name:"ashish",
+//     age:20,
+// }  // object
+
+// const myFunction= function(){
+//     console.log("Hello World");
+// }  // Function
+
+
