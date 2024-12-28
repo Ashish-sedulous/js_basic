ec7b799ee996f292aeba0f6d32ab0575fdac0966
Ashish <ashishkumar12340sss@gmail.com>
2024-12-28 11:14:27 +0000

    maths

---
+++ 01_basics/maths.js
@@ -0,0 +1,28 @@
+const balance= new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+const other=123.8756
+// console.log(other.toPrecision(3));
+// console.log(other.toPrecision(2));
+// console.log(other.toPrecision(4));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-IN'));
+
+// console.log(Math);
+// console.log(Math.abs(-4)); //->absolute value
+// console.log(Math.round(4.6));//->roundoff value
+// console.log(Math.ceil(4.2));//->ceiling value
+// console.log(Math.floor(4.9));//->floor vlaue
+
+// console.log(Math.random());
+// console.log((Math.random()*10)+1);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min=15
+const max=25
+
+console.log(Math.floor(Math.random()*(max-min+1))+min);
