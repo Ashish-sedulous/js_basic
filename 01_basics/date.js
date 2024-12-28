a21582ef3ea4e985152bf6d20900cc36ca654552
Ashish <ashishkumar12340sss@gmail.com>
2024-12-28 18:11:44 +0000

    date

---
+++ 01_basics/date.js
@@ -0,0 +1,19 @@
+let date= new Date()
+// console.log(date);
+// console.log(date.toString());
+// console.log(date.toISOString());
+// console.log(date.toISOString());
+// console.log(date.toJSON());
+// console.log(date.toLocaleDateString());
+
+let mydate=new Date(2004,6,15)
+// console.log(mydate.toDateString());
+// console.log(mydate.toLocaleDateString());
+
+let mydatecreated=new Date("01-14-2023")
+//console.log(mydatecreated.toLocaleString());
+let mytimespan=Date.now()
+
+//console.log(mytimespan);
+//console.log(mydatecreated.getTime());
+console.log(Math.floor(Date.now()/1000));
