e9b9890f23be263cb54349e6a2386e6a52508073
Ashish <ashishkumar12340sss@gmail.com>
2024-12-30 13:03:16 +0000

    execution

---
+++ 03_next/execution.js
@@ -0,0 +1,9 @@
+//javascript execution context
+
+//Global execution context  ->this
+//Function execution context
+//Eval execution context
+
+//code execute in javascript will be done in two phases
+//1.memory creation phase 
+//2.Execution phase
